0e7bf5efb9bcf32ccb3bd7e96a8e8890a687125d
arslonga83 <jlwood83@gmail.com>
2023-01-04 22:19:22 -0800

    hook up search button and log data

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// testing API search movie by title
-// fetch('http://www.omdbapi.com/?apikey=4127127c&s=nemo')
-//   .then(res => res.json())
-//   .then(data => console.log(data))
+const searchBtn = document.querySelector('#search-btn')
+const searchBox = document.querySelector('#search')
+
+searchBtn.addEventListener('click', (e) => {
+  fetch(`http://www.omdbapi.com/?apikey=4127127c&s=${searchBox.value}`)
+  .then(res => res.json())
+  .then(data => console.log(data))
+})
+
+
+
 
