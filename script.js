9df44d2432f53aa8fd637cf1e5c5ccef28e29889
arslonga83 <jlwood83@gmail.com>
2023-01-05 14:58:40 -0800

    draft 2nd api call to get full descriptions

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const searchBox = document.querySelector('#search')
 searchBtn.addEventListener('click', (e) => {
   fetch(`http://www.omdbapi.com/?apikey=4127127c&s=${searchBox.value}`)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data.Search)
+    //get movie ids from results
+    const searchIds = data.Search.map((movie => {
+        return movie.imdbID
+    }))
+    console.log(searchIds)
+    
+  searchIds.map((id) => {
+    fetch(`http://www.omdbapi.com/?apikey=4127127c&i=${id}&plot=short`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  })
+})
+  
 })
 
 
