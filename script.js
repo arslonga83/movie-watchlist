fb0e055cf6b1fbb842887e7be067256936d79b9e
arslonga83 <jlwood83@gmail.com>
2023-01-05 16:48:59 -0800

    added functions to render results and started trying to style the containers

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 const searchBtn = document.querySelector('#search-btn')
 const searchBox = document.querySelector('#search')
+const main = document.querySelector('#main-section') 
+let resultsArray = []
+
 
 searchBtn.addEventListener('click', (e) => {
   fetch(`http://www.omdbapi.com/?apikey=4127127c&s=${searchBox.value}`)
   .then(res => res.json())
   .then(data => {
-    console.log(data.Search)
-    //get movie ids from results
     const searchIds = data.Search.map((movie => {
         return movie.imdbID
     }))
-    console.log(searchIds)
-    
-  searchIds.map((id) => {
-    fetch(`http://www.omdbapi.com/?apikey=4127127c&i=${id}&plot=short`)
-      .then(res => res.json())
-      .then(data => console.log(data))
+    getResultsArray(searchIds)
   })
 })
-  
-})
+
+function getResultsArray(ids) {
+  ids.map((id) => {
+    fetch(`http://www.omdbapi.com/?apikey=4127127c&i=${id}&plot=short`)
+      .then(res => res.json())
+      .then(data => {
+        resultsArray.push(data)
+      })
+  })
+  renderResults()
+}
+
+function renderResults() {
+  let moviesHtml = ''
+  resultsArray.map((movie) => {
+  moviesHtml += `
+  <div class="movie-container">
+    <img src=${movie.Poster}>
+    <div>
+      <div>
+        <h2>${movie.Title}</h2>
+        <p>${movie.imdbRating}</p>
+      </div>
+      <div>
+      <p>${movie.Runtime}</p>
+      <p>${movie.Genre}</p>
+      </div>
+      <p>${movie.Plot}</p>
+    </div>
+  </div>
+  `
+  })
+  main.innerHTML = moviesHtml
+}
 
 
 
